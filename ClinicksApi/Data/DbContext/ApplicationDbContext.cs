6431ef9365d0d70f1b5d2b2a3e9564028193dddc
Tobias <tobiascesarfacundoacevedo@gmail.com>
2026-04-18 21:02:31 -0300

    Limpieza final de temporales

---
+++ ClinicksApi/Data/DbContext/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+// Asegúrate de usar el namespace de tu proyecto
+namespace ClinicksApi.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        // Aquí es donde luego agregaremos los "DbSet" (las tablas como Pacientes, Médicos)
+    }
+}
