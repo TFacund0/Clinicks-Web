c9a482cfa26a56a40ee3cc1e79d386a5e1af6ccc
Tobias <tobiascesarfacundoacevedo@gmail.com>
2026-04-20 21:55:34 -0300

    Implementaci[on completa en el backend para el listado de pacientes

--- ClinicksApi/Business/Interfaces/IPacienteService.cs
+++ ClinicksApi/Business/Interfaces/IPacienteService.cs
@@ -6,7 +6,12 @@ namespace ClinicksApi.Business.Interfaces;
 
 public interface IPacienteService
 {
+    // Método para obtener el listado de pacientes
     Task<IEnumerable<PacienteDto>> ObtenerListado();
-    
+
+    // Método para obtener un paciente por su ID
     Task<PacienteDto?> ObtenerPorId(int id);
+
+    // Método para obtener pacientes atendidos por un médico específico
+    Task<IEnumerable<PacienteDto>> ObtenerAtendidosPorMedico(int medicoId);
 }
