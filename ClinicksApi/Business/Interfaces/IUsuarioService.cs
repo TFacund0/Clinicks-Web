e33906ebbdc9072ef99e7639e44dc693de060bc6
Tobias <tobiascesarfacundoacevedo@gmail.com>
2026-06-25 21:29:56 -0300

    nada

--- ClinicksApi/Business/Interfaces/IUsuarioService.cs
+++ ClinicksApi/Business/Interfaces/IUsuarioService.cs
@@ -13,10 +13,5 @@ namespace ClinicksApi.Business.Interfaces
         /// <param name="username">El nombre de usuario o matrícula.</param>
         /// <param name="password">La contraseña en texto plano para verificar.</param>
         Task<LoginResponseDto?> AutenticarAsync(string username, string password);
-
-        /// <summary>
-        /// Escanea todos los usuarios y encripta con BCrypt las contraseñas en texto plano.
-        /// </summary>
-        Task<int> EncriptarClavesExistentesAsync();
     }
 }
